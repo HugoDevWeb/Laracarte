ad581eedd575f4cbef123060149c87fb30d2324d
Hugo <hugo.lavergne@devinci.fr>
2019-02-16 20:02:43 +0100

    ajoutHelpersTitleDynamic

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,5 +1,7 @@
-@extends('layouts.default')
+@extends('layouts.default', ['title'=>'Home'])
 
 @section('content')
+    <div class="container">
     <h1 class="text-center mb-5">Map here !</h1>
+    </div>
 @stop
