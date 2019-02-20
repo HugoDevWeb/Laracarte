433b6450ea137cd27e55197f63c2ccf8ae5a0005
Hugo <hugo.lavergne@devinci.fr>
2019-02-20 14:15:29 +0100

    MessageUpdate

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Lcobucci\JWT\Builder;
 
 class Message extends Model
 {
     protected $fillable = ['name', 'email', 'message'];
+
 }
